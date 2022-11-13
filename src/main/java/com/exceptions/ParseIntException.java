0c27f246a37ca00dff4e7c50569ccbaecde1ff56
DarwinDemian <darwindemian@hotmail.com>
2022-11-13 02:44:18 -0300

    Migrate to Maven complete

--- src/main/java/com/exceptions/ParseIntException.java
+++
@@ -1,9 +0,0 @@
-package java.com.exceptions;
-
-public class ParseIntException extends Exception {
-    public ParseIntException(Object value) {
-        super(String.format(
-                "Can't convert Integer value '%s' to Integer. Please pass an actual Integer value as a String such as 'intValueAsString' ",
-                value.toString()));
-    }
-}
